bbfcd72f1e1e1340cc42f63795799a3fab8799f0
jobe-lumni <jobecamera@gmail.com>
2023-06-10 08:52:57 -0300

    limpando input apos dar submit

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -13,13 +13,15 @@ const CourseInput = props => {
   const formSubmitHandler = event => {
     event.preventDefault();
     props.onAddGoal(enteredValue);
+    setEnteredValue('');
+
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input type="text" value={enteredValue} onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
